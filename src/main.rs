594c6eaf8e784d5ae6c86728ec54539321373bae
Claus Guttesen <kometen@gmail.com>
2021-01-09 22:53:31 +0100

    Use struct and pass it as a reference.

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,19 @@ extern crate clap;
 use clap::{Arg, App};
 use std::{fs, io};
 
-fn f(path: String) -> io::Result<()> {
+fn f(path: String, arguments: &Arguments) -> io::Result<()> {
     let mut entries = fs::read_dir(path)?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>, io::Error>>()?;
 
-    entries.sort();
+    if arguments.sort {
+        entries.sort();
+    }
 
     for entry in entries {
         if entry.as_path().is_dir() {
             println!("{}/", entry.display());
-            f(entry.display().to_string())?;
+            f(entry.display().to_string(), arguments)?;
         } else {
             println!("{}", entry.display());
         }
@@ -22,13 +24,13 @@ fn f(path: String) -> io::Result<()> {
 }
 
 struct Arguments {
-    path: String,
+    sort: bool,
 }
 
 impl Arguments {
-    fn new(path: String) -> Self {
+    fn new(sort: bool) -> Self {
         Arguments {
-            path,
+            sort,
         }
     }
 }
@@ -47,13 +49,22 @@ fn main() {
             .multiple(false)
             .default_value(".")
         )
+        .arg(Arg::with_name("sort")
+            .help("alphabetical sort")
+            .required(false)
+            .takes_value(false)
+            .short("s")
+            .long("sort")
+            .multiple(false)
+        )
         .get_matches();
 
     let path = matches.value_of("file hierarchy").unwrap();
+    let sort: bool = matches.is_present("sort");
 
-    let arguments = Arguments::new(path.to_string());
+    let arguments = Arguments::new(sort);
 
     println!("find-command in rust");
 
-    let _r = f(arguments.path);
+    let _r = f(path.to_string(), &arguments);
 }
