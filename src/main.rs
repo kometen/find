4569996006cb1aceea5a85e9e89e4e12ab435d1a
Claus Guttesen <kometen@gmail.com>
2021-01-29 08:25:07 +0100

    Use Option() since any number of parameters may or may not apply.

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,47 @@ fn f(path: String, arguments: &Arguments) -> io::Result<()> {
     Ok(())
 }
 
+struct Condition(String, i32);
+
 struct Arguments {
     sort: bool,
+    bmin: Condition,
 }
 
 impl Arguments {
-    fn new(sort: bool) -> Self {
+    fn new(
+        sort: Option<bool>,
+        bmin: Option<Condition>,
+    ) -> Self {
         Arguments {
-            sort,
+            sort: sort.unwrap(),
+            bmin: bmin.unwrap(),
         }
+    }
+}
+
+fn split_parameter(p: &str) -> Condition {
+    if p.starts_with("+") || p.starts_with("-") {
+        let (first, last) = p.split_at(1);
+        let operator = match first {
+            "+" => "gt",
+            "-" => "lt",
+            _  => "eq",
+        };
+        let d: i32 = match last.parse::<i32>() {
+            Ok(n) => n.abs(),
+            Err(_) => -1,
+        };
+        let condition = Condition(operator.to_string(), d);
+        condition
+    } else {
+        let operator = "eq".to_string();
+        let d: i32 = match p.parse::<i32>() {
+            Ok(n) => n.abs(),
+            Err(_) => -1,
+        };
+        let condition = Condition(operator.to_string(), d);
+        condition
     }
 }
 
@@ -61,10 +93,27 @@ fn main() {
             .long("sort")
             .multiple(false)
         )
+        .arg(Arg::with_name("Bmin")
+            .help("Difference between file's inode creation and time find started")
+            .required(false)
+            .takes_value(true)
+            .multiple(false)
+            .max_values(1)
+            .allow_hyphen_values(true)
+            .value_name("[+|-]n")
+            .long("Bmin")
+        )
         .get_matches();
 
     let sort: bool = matches.is_present("sort");
-    let arguments = Arguments::new(sort);
+    let condition: Condition = Condition("a".to_string(), 1);
+    let arguments = Arguments::new(Some(sort), Some(condition));
+
+    if matches.is_present("Bmin") {
+        let p = matches.value_of("Bmin").unwrap();
+        let condition: Condition = split_parameter(p);
+        let arguments = Arguments::new(Some(sort), Some(condition));
+    }
 
     if matches.is_present("file hierarchy") {
         let paths: Vec<_> = matches.values_of("file hierarchy").unwrap().collect();
